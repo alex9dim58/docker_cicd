121e62a0b99ef563b268bbb22fd3e48159809a87
alex9dim58 <77849274+alex9dim58@users.noreply.github.com>
2025-04-08 15:55:03 +0200

    Add kubernetes.yaml

--- src/test/java/org/springframework/samples/petclinic/PostgresIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PostgresIntegrationTests.java
@@ -79,13 +79,15 @@ public class PostgresIntegrationTests {
 	}
 
 	@Test
-	void testFindAll() throws Exception {
+	void testFindAll() throws Exception
+	{
 		vets.findAll();
 		vets.findAll(); // served from cache
 	}
 
 	@Test
-	void testOwnerDetails() {
+	void testOwnerDetails()
+	{
 		RestTemplate template = builder.rootUri("http://localhost:" + port).build();
 		ResponseEntity<String> result = template.exchange(RequestEntity.get("/owners/1").build(), String.class);
 		assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
